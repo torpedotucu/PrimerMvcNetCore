3d2dba69389aa5df5d9e7b0035745eb628e5c21a
torpedotucu <rodrigoorovio2@gmail.com>
2025-01-28 13:50:55 +0100

    xx

--- PrimerMvcNetCore/Controllers/MatematicasController.cs
+++ PrimerMvcNetCore/Controllers/MatematicasController.cs
@@ -27,6 +27,31 @@ namespace PrimerMvcNetCore.Controllers
             ViewData["DATA"]=numero1+" + "+numero2+" = "+suma;
             return View();
         }
+
+        public IActionResult ConjeturaCollatz()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ConjeturaCollatz(int numero)
+        {
+            List<int> numeros = new List<int>();
+            while (numero!=1)
+            {
+                if (numero%2==0)
+                {
+                    numero=numero/2;
+
+                }
+                else
+                {
+                    numero=numero*3+1;
+                }
+                numeros.Add(numero);
+            }
+            return View(numeros);
+        }
     }
 
 }
